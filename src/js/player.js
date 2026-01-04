dc9577d41a2ca5fbc715437fea6705b58dd3c26f
Fung <koufongso@gmail.com>
2026-01-03 20:03:51 -0800

    refactor: separate player data from game logic services

--- src/js/player.js
+++ src/js/player.js
@@ -1,68 +1,69 @@
 import { ResourceType } from './map/resource_type.js';
 
 export class Player {
-    constructor(name) {
+    constructor(id, name, color) {
+        this.id = id;
         this.name = name;
-        this.resources = new Map();
-        this.resources.set(ResourceType.WOOD, 0);
-        this.resources.set(ResourceType.BRICK, 0);
-        this.resources.set(ResourceType.SHEEP, 0);
-        this.resources.set(ResourceType.WHEAT, 0);
-        this.resources.set(ResourceType.ROCK, 0);
+        this.color = color;
 
-        this.roads = new Map(); // list of road objects owned by the player
-        this.settlements = new Map(); // list of settlement objects owned by the player
-        this.publicSpeicalScore = {"ArmyCrop":0, "longestRoad":0,"Knight":0}; // additional score from special cards
-        this.hiddenSpecialCards = {"Knight":0, "SpecialScore":0}; // list of special cards owned by the player
+        // resource inventory
+        this.resources = new Map();
+        // initialize all resource counts to zero
+        Object.values(ResourceType).forEach(type => {
+            if (type !== ResourceType.DESERT) {
+                this.resources.set(type, 0);
+            }
+        });
+
+        // asset ownership
+        this.settlementIds = new Set();
+        this.roadIds = new Set();
+
+        // Achievement/Special Status (Data only, logic handled by ScoreService)
+        this.achievements = {
+            hasLongestRoad: false,
+            hasLargestArmy: false,
+            victoryPointCards: 0,
+            knightsPlayed: 0
+        };
     }
 
-    // method to update player's resource
-    // type: resource type (from ResourceType)
-    // amount can be positive (gain) or negative (spend)
+    // cost is an object {resourceType: amount, ...}
+    canAfford(cost) {
+        for (let [type, amount] of Object.entries(cost)) {
+            if (this.resources.get(type) < amount) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // update resource count
     updateResource(type, amount) {
         if (this.resources.has(type)) {
-            this.resources.set(type, this.resources.get(type) + amount);
+            const current = this.resources.get(type);
+            // Prevents resources from dropping below zero
+            this.resources.set(type, current + amount);
         }
     }
 
-    // compute the latest score based on settlements and cities
-    getScore(){
-        let score = 0;
 
-        for (const settlement of this.settlements.values()) {
-            console.log(settlement);
-            score += settlement.getScoreValue();
-        }
-
-        for (const bonus of Object.values(this.bounusScore)) {
-            score += bonus;
-        }
-
-        return score;
+    // add a settlement object to player's list
+    addSettlement(vertexId) {
+        this.settlementIds.add(vertexId);
     }
 
-    getSettlements(){
-        return this.settlements;
+    // add a road object to player's list
+    addRoad(edgeId) {
+        this.roadIds.add(edgeId);
     }
 
-    getRoads(){
-        return this.roads;
-    }
-
-    addSettlement(settlement){
-        this.settlements.set(settlement.vertex.id, settlement);
-    }
-
-    upgradeSettlementByCoord(coord){
-        let id = `${coord[0]},${coord[1]},${coord[2]}`;
-        if (this.settlements.has(id)) {
-            try{
-                this.settlements.get(id).upgrade();
-            }catch(error){
-                console.error(error);
-            }
-        }else{
-            throw new Error(`No settlement found with id: ${id}`);
-        }
+    // helper function to get all owned assets ids
+    getOwnedAssets() {
+        return {
+            settlements: Array.from(this.settlementIds),
+            roads: Array.from(this.roadIds)
+        };
     }
 }
