fde990b524855fdb3ff35865e9eed437ade46a40
Fung <koufongso@gmail.com>
2026-01-04 13:56:03 -0800

    feat: add GameEngine and UI components for game initialization and rendering

---
+++ src/js/game_engine.js
@@ -0,0 +1,33 @@
+import { GameController } from "./game_controller.js";
+import { Renderer } from "./renderer.js";
+
+// Main Game Engine class
+export class GameEngine{
+    constructor(){
+        // game engine setup
+        this.gameController = new GameController();
+        this.renderer = new Renderer('map-svg');
+        this.gameController.attachRenderer(this.renderer);
+        this.renderer.attachCOntroller(this.gameController);
+    }
+
+    async run(){
+        // start the game engine
+        console.log("Starting Game Engine...");
+
+        // manual trigger to start the game for testing
+        await this.gameController.inputEvent({
+            type: 'START_GAME',
+            humanPlayers: 1,
+            aiPlayers: 1,
+            seed: 12345
+        });
+
+        // further game loop logic would go here
+        console.log("Game Engine is running.");
+    
+    }
+}
+
+let gameEngine = new GameEngine();
+await gameEngine.run();
